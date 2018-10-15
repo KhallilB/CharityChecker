c31800239567e07a07b0f80a695c907eb83869c4
KhallilB <khallil.bailey@gmail.com>
2018-10-15 13:01:37 -0700

    fixing

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const express = require('express');
 const exphbs =  require('express-handlebars');
 const methodOverride = require("method-override");
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
